d7a226a3a055994261db8ad1e1700580db7124ac
ThanuGeorge <71665970+ThanuGeorge@users.noreply.github.com>
2020-11-19 23:52:48 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,24 @@
+class Plinko {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        noStroke();
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y,this.r,this.r);
+    
+    }
+
+};
